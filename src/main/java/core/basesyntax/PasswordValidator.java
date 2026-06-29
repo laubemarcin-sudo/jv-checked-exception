68261b1d10b0843d4986201defa849d6b951f50d
Marcin Laube <laubemarcin2gmail.com>
2026-06-29 17:24:13 +0200

    password

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -1,9 +1,12 @@
 package core.basesyntax;
 
 public class PasswordValidator {
+
     public void validate(String password, String repeatPassword) throws PasswordValidationException {
-        if (!password.equals(repeatPassword) || password.length() < 10)
-          throw new PasswordValidationException("Wrong passwords");
-        //write your code here
+        if (password == null || repeatPassword == null
+                || !password.equals(repeatPassword)
+                || password.length() < 10) {
+            throw new PasswordValidationException("Wrong passwords");
+        }
     }
 }
